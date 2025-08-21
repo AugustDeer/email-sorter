4ff7427f10b2053851b853c933fd0e2079b6c7cb
August Deer <august@augustdeer.com>
2025-08-21 09:50:05 -0700

    Only train classifier head

--- src/email_sorter/__init__.py
+++ src/email_sorter/__init__.py
@@ -3,33 +3,39 @@ import logging
 import evaluate
 import numpy as np
 import torch as t
-from accelerate import Accelerator
-from datasets import Dataset, DatasetDict, load_dataset
+from datasets import load_dataset
+from torch.utils.data import Dataset
 from transformers import (
-    AutoModelForSequenceClassification,
     AutoTokenizer,
-    PreTrainedModel,
+    ModernBertForSequenceClassification,
     PreTrainedTokenizerBase,
     Trainer,
     TrainingArguments,
 )
+from transformers.modeling_outputs import SequenceClassifierOutput
 
 logger: logging.Logger = logging.getLogger(__name__)
 
-accelerator: Accelerator = Accelerator()
-device: t.device = accelerator.device
 
-
-def fetchModel(model_name: str) -> tuple[PreTrainedModel, PreTrainedTokenizerBase]:
+def fetchModel(
+    model_name: str,
+) -> tuple[ModernBertForSequenceClassification, PreTrainedTokenizerBase]:
     """Initializes the specified model and associated tokenizer"""
 
-    tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(model_name)
-    model: PreTrainedModel = AutoModelForSequenceClassification.from_pretrained(
-        model_name,
-        num_labels=2,
-        device_map="auto",
-        torch_dtype=t.float16,
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model: ModernBertForSequenceClassification = (
+        ModernBertForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=2,
+            device_map="auto",
+            id2label={0: "ham", 1: "spam"},
+            label2id={"ham": 0, "spam": 1},
+        )
     )
+
+    # Freeze BERT weights
+    for layer in model.model.parameters():
+        layer.requires_grad = False
 
     return model, tokenizer
 
@@ -40,61 +46,57 @@ def fetchData(
     """Imports the specified dataset and tokenizes it."""
 
     # Import dataset
-    dataset: DatasetDict = load_dataset(dataset_name)  # pyright: ignore[reportAssignmentType]
+    dataset = load_dataset(dataset_name)
 
     # Process dataset
     def tokenize(batch: dict[str, list[str]]) -> dict[str, list[str]]:
-        return tokenizer(batch["text"], padding="max_length", truncation=True).data
+        return tokenizer(batch["subject"], padding="max_length", truncation=True).data
 
     dataset = dataset.map(tokenize, batched=True)
 
-    return dataset["train"], dataset["test"]
+    return dataset["train"], dataset["test"]  # type: ignore
 
 
-def shrinkData(data: Dataset, size: int, seed: int = 42) -> Dataset:
-    """Randomly select elements from the dataset to reduce size."""
-    return data.shuffle(seed=seed).select(range(size))
-
-
-def prepTrainer(model: PreTrainedModel, train: Dataset, eval: Dataset) -> Trainer:
+def prepTrainer(
+    model: ModernBertForSequenceClassification,
+    tokenizer: PreTrainedTokenizerBase,
+    train: Dataset,
+    eval: Dataset,
+) -> Trainer:
     """Creates a Trainer object for the specified model and data."""
     # Define training arguments
     training_args = TrainingArguments(
-        eval_strategy="epoch",
-        per_device_train_batch_size=4,
-        gradient_accumulation_steps=16,
-        batch_eval_metrics=True,
-        per_device_eval_batch_size=4,
+        per_device_train_batch_size=64,
         dataloader_pin_memory=True,
-        dataloader_num_workers=4,
-        torch_empty_cache_steps=8,
-        # torch_compile=True,
-        # torch_compile_backend="inductor",
+        dataloader_num_workers=6,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        output_dir="./output/trained_model",
     )
 
     metric = evaluate.load("accuracy")
 
-    def compute_metrics(eval_pred, compute_result: bool = True) -> dict | None:
-        logits, labels = eval_pred
-        predictions = t.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=labels)
-        if compute_result:
-            return metric.compute()
-
     trainer = Trainer(
         model=model,
         args=training_args,
+        processing_class=tokenizer,
         train_dataset=train,
         eval_dataset=eval,
-        compute_metrics=compute_metrics,  # pyright: ignore[reportArgumentType]
+        compute_metrics=lambda eval_pred: metric.compute(
+            predictions=np.argmax(eval_pred.predictions, -1),
+            references=eval_pred.label_ids,
+        )
+        or {},
     )
 
     return trainer
 
 
 def testModel(
-    model: PreTrainedModel, tokenizer: PreTrainedTokenizerBase, data: list[str]
-) -> list[str]:
+    model: ModernBertForSequenceClassification,
+    tokenizer: PreTrainedTokenizerBase,
+    data: list[str],
+) -> t.Tensor:
     """Test model on given strings."""
     logger.info(f"Testing model on {len(data)} items.")
 
@@ -102,13 +104,12 @@ def testModel(
         data,
         return_tensors="pt",
         padding=True,
-    ).to(device)
+    ).to(model.device)
 
     with t.no_grad():
-        outputs = model(**inputs)
+        outputs: SequenceClassifierOutput = model(**inputs)
 
-    logits: np.ndarray = outputs.logits.to("cpu")
-
-    predictions: np.ndarray = np.argmax(logits, axis=-1)
-
-    return predictions.tolist()
+    logits = outputs.logits
+    assert logits is not None
+    predictions = logits.softmax(-1)
+    return predictions.cpu()
--- train-enron.py
+++ train-enron.py
@@ -1,21 +1,25 @@
 # %%
-from email_sorter import fetchData, fetchModel, prepTrainer, shrinkData, testModel
+import torch as t
+
+from email_sorter import fetchData, fetchModel, prepTrainer, testModel
 
 # %%
 model, tokenizer = fetchModel("answerdotai/ModernBERT-base")
 train, eval = fetchData("SetFit/enron_spam", tokenizer)
 
 # %%
-small_train = shrinkData(train, 5000)
-small_eval = shrinkData(eval, 500)
-
-# %%
-trainer = prepTrainer(model, small_train, small_eval)
+trainer = prepTrainer(model, tokenizer, train, eval)
 trainer.train()
+
+model.eval()
+with t.no_grad():
+    metrics = trainer.evaluate()
+    print("Accuracy:", metrics["eval_accuracy"])
 
 # %%
 test = ["Save 50% on new backpacks!", "Reminder: Homework due tomorrow."]
 predictions = testModel(model, tokenizer, test)
 
 for d, p in zip(test, predictions):
-    print(f"Input: {d}", f"Prediction: {'spam' if p else 'ham'}", sep="\n", end="\n\n")
+    print(f"Input: {d}")
+    print(*(f"{k} {p[v]}" for k, v in model.config.label2id.items()))
